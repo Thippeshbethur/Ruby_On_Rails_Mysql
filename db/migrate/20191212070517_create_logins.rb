92e82c44834eefe81831168a2183780b1ec611d8
Thippesh B N <[thippesh.bn@excelindia.com]>
2019-12-18 11:14:45 +0530

    Project

---
+++ db/migrate/20191212070517_create_logins.rb
@@ -0,0 +1,10 @@
+class CreateLogins < ActiveRecord::Migration[6.0]
+  def change
+    create_table :logins do |t|
+      t.string:loginname
+      t.text:password
+
+      t.timestamps
+    end
+  end
+end
